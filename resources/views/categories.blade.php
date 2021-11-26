3eb68aec508fef273c1de35c29f1b98f34deb269
zmantas17 <mantas.zemaitis.0427@stud.kitm.lt>
2021-11-26 15:55:51 +0200

    edited category dashboard view

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -7,12 +7,15 @@
 
     <div class="justify-content-center">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="text-right mt-4 max-w-7xl"> 
+                <a href="/add-category" class="btn btn-success ">Add <i class="fas fa-plus-circle"></i></a>
+            </div>
             <div class="overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="mt-5 border-b border-gray-200">
+                <div class="mt-3 border-b border-gray-200">
                     <table class="table table-hover table-dark">
                         <thead>
                             <tr class=" border border-bottom-0">
-                                <th scope="col">Name</th>
+                                <th scope="col">Title</th>
                                 <th scope="col mr-3">Action</th>
                             </tr>
                         </thead>
@@ -22,9 +25,9 @@
                                 {{-- <td>{{App\Models\User::where('id', $category->owner)->get()[0]->name}}</td> --}}
                                 <td>{{$category->name}}</td>
                                 <td>
-                                    <a href="/add-category/" class="btn btn-success border border-warning border-2 border-0"><i class="fas fa-plus"></i></a>
-                                    <a href="/category/{{ $category->id }}/edit" class="btn btn-warning border border-warning border-2 border-0"><i class="fas fa-pencil-alt"></i></a>
-                                    <a href="/category/{{ $category->id }}/delete/ask" class="btn btn-danger border border-warning border-2 border-0"><i class="fas fa-trash"></i></a>
+                                    <a href="/category/{{ $category->id }}" class="btn btn-success"><i class="fas fa-arrow-alt-circle-right"></i></a>
+                                    <a href="/category/{{ $category->id }}/edit" class="btn btn-warning"><i class="fas fa-pencil-alt"></i></a>
+                                    <a href="/category/{{ $category->id }}/delete/ask" class="btn btn-danger"><i class="fas fa-trash"></i></a>
                                 </td>
                             </tr>
                         @endforeach
